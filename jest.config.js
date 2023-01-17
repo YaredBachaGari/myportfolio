1f8be2be78649d528eb14dd4d0d8f3e0628894a9
YaredBG <Yared_Gari@Mckinsey.com>
2023-01-17 17:40:41 -0500

    ci: implemented test coverage criteria

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
-  globals:{
-    'ts-jest':{
-      tsconfig: './tsconfig.jest.json'
-    }
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.jest.json',
+    },
   },
-  setupFilesAfterEnv:['./src/jest.setup.ts'],
+  setupFilesAfterEnv: ['./src/jest.setup.ts'],
   coverageThreshold: {
     global: {
       branches: 80,
